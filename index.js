5ca8e05d79bcb78cfc4d987c8b1e5e1187f485a6
Jac Wang <lu7766lu7766@gmail.com>
2025-07-23 15:25:22 +0800

    p1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("hello world!")
+0.5
+1
+;("use strict")
+;("jhhjgj")
+true
+false
+
+// let number = 2 // .toFixed(2) // "2.00" 2 "2.01" 2.01
+// const name = "jac"
+// number = 3
+// let temp = number
+// console.log(temp)
+// number = 4
+// temp = number
+// console.log(name)
+
+// practice
+// let a = 10
+// let b = 20
+// let c
+// c = a
+// a = b
+// b = c
+// //
+// console.log(a, b)
